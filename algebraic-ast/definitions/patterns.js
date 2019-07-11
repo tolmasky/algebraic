c1d7cd934f0b58ff83bc73eb8989c5e6a148428f
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-11 11:53:29 -0700

    Update patterns.
    Reviewed by @tolmasky.

--- algebraic-ast/definitions/patterns.js
+++ algebraic-ast/definitions/patterns.js
@@ -1,15 +1,11 @@
 const { is, data, or, boolean, string, type, array } = require("@algebraic/type");
 const union2 = require("@algebraic/type/union-new");
-
-
-const always = value => [type.of(value), () => value];
-
 const Node = require("./node");
 const Bindings = require("./string-set").in `bindings`;
 const FreeVariables = require("./string-set").in `freeVariables`;
 
 
-exports.RootPattern =   union2 `RootPattern` (
+exports.RootPattern = union2 `RootPattern` (
     is                  =>  Node.IdentifierPattern,
     or                  =>  Node.ArrayPattern,
     or                  =>  Node.ObjectPattern );
@@ -17,37 +13,31 @@ exports.RootPattern =   union2 `RootPattern` (
 // In order to maintain backwards compatibility with ESTree's spec, we set
 // the type property to "Identifier". This makes this portion of the tree
 // appear unchanged to Babel and other ESTree compatible tools.
-exports.IdentifierPattern = data `IdentifierPattern` (
-    ([type])            =>  always ("Identifier"),
+exports.IdentifierPattern = Node `IdentifierPattern` (
+    ({override:type})   => "Identifier",
 
     name                =>  string,
     ([bindings])        =>  Bindings.lift("name"),
     ([freeVariables])   =>  FreeVariables.lift("name") );
 
 exports.RestElement = data `RestElement` (
-    ([type])            =>  always ("RestElement"),
-
     argument            =>  Node.RootPattern,
     ([bindings])        =>  Bindings.from("argument"),
     ([freeVariables])   =>  FreeVariables.from("argument") );
 
 exports.AssignmentPattern = data `AssignmentPattern` (
-    ([type])            =>  always ("AssignmentPattern"),
-
     left                =>  Node.RootPattern,
     right               =>  Node.Expression,
     ([bindings])        =>  Bindings.from("left"),
     ([freeVariables])   =>  FreeVariables.from("left", "right") )
 
 exports.ArrayPattern = data `ArrayPattern` (
-    ([type])            =>  always ("ArrayPattern"),
-
     elements            =>  array (or (Node.RootPattern, Node.AssignmentPattern)),
     ([bindings])        =>  Bindings.from("elements"),
     ([freeVariables])   =>  FreeVariables.from("elements") );
 
 exports.ShorthandAssignmentPattern = data `ShorthandAssignmentPattern` (
-    ([type])            =>  always ("AssignmentPattern"),
+    ({override:type})   =>  "AssignmentPattern",
 
     left                =>  Node.IdentifierPattern,
     right               =>  Node.Expression,
@@ -55,10 +45,10 @@ exports.ShorthandAssignmentPattern = data `ShorthandAssignmentPattern` (
     ([freeVariables])   =>  FreeVariables.from("left", "right") );
 
 exports.ObjectPropertyPatternShorthand = data `ObjectPropertyPatternShorthand` (
-    ([type])            =>  always ("ObjectProperty"),
+    ({override:type})   =>  "ObjectProperty",
 
-    ([shorthand])       =>  always (true),
-    ([computed])        =>  always (false),
+    ([shorthand])       =>  data.always (true),
+    ([computed])        =>  data.always (false),
 
     ([key])             =>  [Node.PropertyName, value =>
                                 is (Node.IdentifierPattern, value) ?
@@ -71,9 +61,9 @@ exports.ObjectPropertyPatternShorthand = data `ObjectPropertyPatternShorthand` (
     ([freeVariables])   =>  FreeVariables.from("value") );
 
 exports.ObjectPropertyPatternLonghand = data `ObjectPropertyPatternLonghand` (
-    ([type])            =>  always ("ObjectProperty"),
+    ({override:type})   =>  "ObjectProperty",
 
-    ([shorthand])       =>  always (false),
+    ([shorthand])       =>  data.always (false),
     ([computed])        =>  [boolean, key =>
                                 is(Node.ComputedPropertyName, key)],
 
@@ -91,8 +81,6 @@ exports.ObjectPropertyPattern = union2 `ObjectPropertyPattern` (
     or                  => Node.ObjectPropertyPatternShorthand );
 
 exports.ObjectPattern = data `ObjectPattern` (
-    ([type])            =>  always ("ObjectPattern"),
-
     properties          =>  array (Node.ObjectPropertyPattern ),
     ([bindings])        =>  Bindings.from("properties"),
     ([freeVariables])   =>  FreeVariables.from("properties") );
