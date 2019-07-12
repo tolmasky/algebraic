ed410b6d83fe04aa0587d9681d50b18ee45beec9
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-11 20:27:02 -0700

    Use compute.
    Reviewed by @tolmasky.

--- algebraic-ast/definitions/literals.js
+++ algebraic-ast/definitions/literals.js
@@ -1,36 +1,37 @@
 const { data, nullable, array } = require("@algebraic/type");
 const { boolean, number, string, tundefined } = require("@algebraic/type/primitive");
 const Node = require("./node");
-const FreeVariables = require("./string-set").in `freeVariables`;
+const { StringSet } = require("./string-set");
+const compute = require("./compute");
 const Extra = require("./extra");
 
 
 exports.BigIntLiteral = Node `BigIntLiteral` (
-    value               => string,
-    extra               => [nullable(Extra(string)), null],
-    ([freeVariables])   => FreeVariables.Never );
+    value               =>  string,
+    extra               =>  [nullable(Extra(string)), null],
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.BooleanLiteral = Node `BooleanLiteral` (
     value               =>  boolean,
-    ([freeVariables])   =>  FreeVariables.Never );
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.NumericLiteral = Node `NumericLiteral` (
     value               =>  number,
-    ([freeVariables])   =>  FreeVariables.Never );
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.NullLiteral = Node `NullLiteral` (
-    ([freeVariables])   =>  FreeVariables.Never );
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.RegExpLiteral = Node `RegExpLiteral` (
     flags               =>  string,
     pattern             =>  string,
     extra               =>  [nullable(Extra(tundefined)), null],
-    ([freeVariables])   =>  FreeVariables.Never );
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.StringLiteral = Node `StringLiteral` (
     value               =>  string,
     extra               =>  [nullable(Extra(string)), null],
-    ([freeVariables])   =>  FreeVariables.Never );
+    ([freeVariables])   =>  compute.empty (StringSet) );
 
 exports.TemplateElement = Node `TemplateElement` (
     value               =>  Node.TemplateElement.Value,
@@ -43,7 +44,8 @@ exports.TemplateElement.Value = data `TemplateElement.Value` (
 exports.TemplateLiteral = Node `TemplateLiteral` (
     expressions         =>  array(Node.Expression),
     quasis              =>  array(Node.TemplateElement),
-    ([freeVariables])   =>  FreeVariables.from("expressions") );
+    ([freeVariables])   =>  comput (StringSet,
+                                take => `expressions` ) );
 
 
 
