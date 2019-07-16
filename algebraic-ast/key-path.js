8ee2dbac2c325a3048956c0a605a0330a33582dc
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-16 11:45:45 -0700

    Fix KeyPath.
    Reviewed by @tolmasky.

--- algebraic-ast/key-path.js
+++ algebraic-ast/key-path.js
@@ -1,4 +1,4 @@
-const { data, string, union } = require("@algebraic/type");
+const { is, data, string, union } = require("@algebraic/type");
 const { Map, List } = require("@algebraic/collections");
 
 const KeyPath = union `KeyPath` (
@@ -32,7 +32,7 @@ KeyPath.Parent.prototype[Symbol.iterator] = function * ()
 KeyPath.Root.prototype.toString =
 KeyPath.Parent.prototype.toString = function ()
 {
-    return `[${Array.from(this).join(", ")}]`;
+    return `@[${Array.from(this).join(", ")}]`;
 }
 
 const fNameRegExp = /([^=\s]+)\s*=>/;
@@ -42,36 +42,36 @@ const has = hasOwnProperty.call.bind(hasOwnProperty);
 const { field } = data;
 const { isArray } = Array;
 
-const push = (...keys) =>
-    keyPaths =>
-        keyPaths.map(keyPath => keys
-            .reduceRight((child, key) =>
-                KeyPath.Parent({ key, child }), keyPath));
+const push = (...keys) => keyPaths =>
+    keyPaths.map(keyPath => keys
+        .reduceRight((child, key) =>
+            KeyPath.Parent({ key, child }), keyPath));
+const take = (lhs, rhs) =>
+    lhs.size === 0 ? rhs :
+    rhs.size === 0 ? lhs :
+    rhs.reduce((accum, keyPaths, name) =>
+        accum.update(name, KeyPaths(),
+            existing => existing.concat(keyPaths),
+        lhs));
 
 const inKeyPath = (value, keys, index = 0) =>
-    index === keys.length ?
-        typeof value === "string" ?
-            KeyPathsByName({ [value]: KeyPaths([KeyPath.Root]) }) :
-            value :
-        (child =>
-            !child ? None :
-            !isArray(child) ?
-                inKeyPath(child, keys, index + 1)
-                    .map(push(keys[index])) :
-            child.reduce((keyPathsByName, item, location) =>
-                inKeyPath(item, keys, index + 1)
-                    .map(push(keys[index], location + ""))
-                    .reduce((accum, keyPaths, name) =>
-                        accum.update(name, KeyPaths(), 
-                            existing => existing.concat(keyPaths)),
-                        keyPathsByName),
-                    None))(value[keys[index]]);
+    (child =>
+        !child ? None :
+        index === keys.length - 1 ?
+            typeof child === "string" ?
+                KeyPathsByName({ [child]: KeyPaths([KeyPath.Root]) }) :
+                child :
+        !isArray(child) ?
+            inKeyPath(child, keys, index + 1).map(push(keys[index])) :
+            child
+                .map((item, iteration) =>
+                    inKeyPath(item, keys, index + 1)
+                        .map(push(keys[index], iteration + "")))
+                .reduce(take, None))(value[keys[index]]);
 
 
 KeyPathsByName.compute = function (...shorthandOperations)
 {
-    const take = (accum, taken, key) =>
-        accum.concat(taken.map(value => KeyPath.Parent(key, value)));
     const subtract = (accum, subtracted) =>
         accum.filter(name => subtracted.has(name));
     const methods = { take, subtract };
