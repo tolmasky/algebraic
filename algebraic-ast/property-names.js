c48878dedb5f88e2e9dc7a4c53e5914d4c0371a9
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2021-02-19 14:02:27 -0800

    Remove unused code in property-names.js.
    Reviewed by @tolmasky.

--- algebraic-ast/property-names.js
+++ algebraic-ast/property-names.js
@@ -1,25 +1,7 @@
 const { string, or } = require("@algebraic/type");
 const union = require("@algebraic/type/union-new");
 const Node = require("./node");
-const { KeyPathsByName } = require("./key-path");
 
-
-// Babel has no concept of a ComputedProperyName at all, and so this would
-// require significant conversion on the way back (unlike PropertyName which
-// just pretends to be an Identifier). However, we can do something similar
-// here and pretend to be a ParenthesizedExpression.
-/*exports.ComputedPropertyName = Node `ComputedPropertyName` (
-    ([type])            =>  data.always ("ParenthesizedExpression"),
-
-    expression          =>  Node.Expression,
-    ([freeVariables])   =>  KeyPathsByName.compute (
-                                take => `expression.freeVariables` ) );
-
-exports.PropertyName = Node `PropertyName` (
-    ([type])            =>  data.always ("Identifier"),
-
-    name                =>  string,
-    ([freeVariables])   =>  data.always (KeyPathsByName.None) );*/
 
 exports.PropertyName = union `PropertyName` (
     is                  =>  Node.LiteralPropertyName,
