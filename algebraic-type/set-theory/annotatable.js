b34aa79978ca70ff92d6ad7a85ea9cc328d962a2
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2021-03-29 10:17:19 -0700

    Better nullable regular expressions.
    Reviewed by @tolmasky.

--- algebraic-type/set-theory/annotatable.js
+++ algebraic-type/set-theory/annotatable.js
@@ -1,4 +1,4 @@
-const AnnotationsRegExp = /^(\??)(=|\(\)=)?$/;
+const AnnotationsRegExp = /^\s*(\??)\s*(=|\s*\(\s*\)\*=)\s*?$/;
 const Set = require("./set");
 const f = require("./f-construct");
 const { isTaggedCall, tagResolve } = require("./templating");
@@ -8,7 +8,7 @@ const given = f => f();
 const range = length => Array.from({ length });
 const parse = (Ai, annotations) => given((
     [, nulls, fallback = ""] = annotations.match(AnnotationsRegExp),
-    nullabled = Set.Union(Ai, Set.null)) =>
+    nullabled = nulls ? Set.Union(Ai, Set.null) : Ai) =>
         fallback ?
             nullabled :
             nullabled);
@@ -16,10 +16,10 @@ const parse = (Ai, annotations) => given((
 const annotatable = (name, initialize = x => x) => 
     f.constructible `${name}` ((A, ...arguments) =>
         initialize(f (
-            (instance, ...arguments2) =>
-                isTaggedCall(arguments2) ?
-                    parse(instance, tagResolve(...arguments2)) :
-                    (()=>{console.log(arguments2); throw `OH No! ${arguments2}`})(),
+            (instance, ...arguments) =>
+                isTaggedCall(arguments) ?
+                    parse(instance, tagResolve(...arguments)) :
+                    (()=>{console.log(arguments); throw `OH No! ${arguments}`})(),
             A.prototype),
             ...arguments));
 
